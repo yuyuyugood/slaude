31a191dba6fd95ffaa44a7d7c208c0fbce9b86ef
Aisu Wata <aisu.wata0@gmail.com>
2023-05-15 14:12:16 -0300

    Added explanation to the config

--- config.js
+++ config.js
@@ -6,11 +6,17 @@ const config = {
     CLAUDE_USER: "U1234A568BC",
 
     // The final message we send with an @Claude ping to trigger his response. You don't need to add the @Claude bit, Slaude does that automatically.
+    // This goes just after your Jailbreak, it goes into its own "Human:" chat reply, instead of appended to the last prompt like JB
     // Can be whatever you want it to be but keep in mind that it might result in unpredictable responses.
     // Anything we put here will eat into our max available tokens so keep it brief. Anything above ~12000 characters will get cut off by Slack.
-    PING_MESSAGE_PREFIX: ".\n\n�",
+    PING_MESSAGE_PREFIX: ".\n\n�A",
     PING_MESSAGE: "r�",
     minimum_split_size: 500,
+    // The "Claude" string is necessary for the ping as said above, but it reminds Claude of who it is
+    // You can muddle that by, for example, adding 'A' to the end of the prefix, and 'r' to the start of the suffix
+    // Which makes the text AClauder, which has a different token breakdown than Claude (Cla+ude -> AC+l+auder)
+    // The weird � character is less supported, but the thought process was to make the thing seem like a leftover error and for the model to ignore it, "AClauder" doesn't make any sense after all. You can remove it to try it out or if you think anything weird is happening, but felt good to me.
+    // Either way I strongly suggest not using ":" as it confuses it with the usual H: and A:
 
     // Claude specs:
     // Human, Assistant, H, A
